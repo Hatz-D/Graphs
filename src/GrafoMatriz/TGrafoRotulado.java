ee89020b7521c726742838610d6d7a8eab6363aa
nicolasmelnik <nmelnik58@gmail.com>
2024-09-24 23:11:55 -0300

    Atualizando

--- src/GrafoMatriz/TGrafoRotulado.java
+++ src/GrafoMatriz/TGrafoRotulado.java
@@ -1,6 +1,7 @@
 /*
-Diogo Lourenzon Hatz     - 10402406
-Nicolas Fernandes Melnik - 10402170
+Diogo Lourenzon Hatz     - RA: 10402406
+Eduardo Marui de Camargo - RA: 10400734
+Nicolas Fernandes Melnik - RA: 10402170
 
 Classe: Classe relativa ao grafo direcionado com peso nas arestas
 
@@ -9,6 +10,7 @@ Mudanças:
 - Separação das classes de grafo em classes distintas e criação de classe abstrata Grafos- Diogo Hatz, 11/09/2024
 - Criação da classe Projeto para métodos de manipulação de arquivos - Diogo Hatz, 11/09/2024
 - Garantir consistência dos métodos das classes dos grafos - Nicolas Melnik 13/09/2024
+- Adicionando mapeamento da europa no arquivo grafo.txt - Nicolas Melnik e Eduardo Marui 24/09/2024
 */
 
 package GrafoMatriz;
@@ -17,11 +19,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class TGrafoRotulado extends Grafo{
-    private double[][] adj; //matriz de adjacência
+public class TGrafoRotulado extends Grafo {
+    private double[][] adj; // matriz de adjacência
 
     // Métodos Públicos
-    public TGrafoRotulado(int n) {  // construtor
+    public TGrafoRotulado(int n) { // construtor
         this.n = n;
         // No início dos tempos não há arestas
         this.m = 0;
@@ -30,8 +32,8 @@ public class TGrafoRotulado extends Grafo{
         this.nomes = new String[n];
 
         // Inicia a matriz com infinitos
-        for(int i = 0; i< n; i++)
-            for(int j = 0; j< n; j++)
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
                 this.adj[i][j] = Double.POSITIVE_INFINITY;
     }
 
@@ -41,63 +43,71 @@ public class TGrafoRotulado extends Grafo{
         this.nomes = new String[n];
         int count = 0;
 
-        for(int i = 0; i < adj.length; ++i) {
-            for(int j = 0; j < adj[i].length; j++) {
-                if(adj[i][j] != Double.POSITIVE_INFINITY) {count++;}
+        for (int i = 0; i < adj.length; ++i) {
+            for (int j = 0; j < adj[i].length; j++) {
+                if (adj[i][j] != Double.POSITIVE_INFINITY) {
+                    count++;
+                }
             }
         }
 
         this.m = count;
     }
 
-    public double[][] getAdj() {return adj;}
+    public double[][] getAdj() {
+        return adj;
+    }
 
     // Insere uma aresta no Grafo tal que
     // v é adjacente a w
     public void insereA(int v, int w, double p) {
         // testa se nao temos a aresta
-        if(adj[v][w] == Double.POSITIVE_INFINITY){
+        if (adj[v][w] == Double.POSITIVE_INFINITY) {
             adj[v][w] = p;
             m++; // atualiza qtd arestas
         }
 
-        //else{System.out.println("\nAresta já existente!");}
+        // else{System.out.println("\nAresta já existente!");}
     }
 
     // remove uma aresta v->w do Grafo
     public void removeA(int v, int w) {
         // testa se temos a aresta
-        if(adj[v][w] != Double.POSITIVE_INFINITY){
+        if (adj[v][w] != Double.POSITIVE_INFINITY) {
             adj[v][w] = Double.POSITIVE_INFINITY;
             m--; // atualiza qtd arestas
         }
 
-        else{System.out.println("\nAresta não existente!");}
+        else {
+            System.out.println("\nAresta não existente!");
+        }
     }
+
     // Apresenta o Grafo contendo
     // número de vértices, arestas
     // e a matriz de adjacência obtida
     public void show() {
-        System.out.println("V: " + n );
-        System.out.println("A: " + m );
-        for(int i=0; i < n; i++){
+        System.out.println("V: " + n);
+        System.out.println("A: " + m);
+        for (int i = 0; i < n; i++) {
             System.out.print("\n");
-            for(int w = 0; w < n; w++)
-                if(adj[i][w] != Double.POSITIVE_INFINITY)
+            for (int w = 0; w < n; w++)
+                if (adj[i][w] != Double.POSITIVE_INFINITY)
                     System.out.print("Adj[" + i + "," + w + "]= " + adj[i][w] + " ");
-                else System.out.print("Adj[" + i + "," + w + "]= ∞" + " ");
+                else
+                    System.out.print("Adj[" + i + "," + w + "]= ∞" + " ");
         }
-        System.out.println("\n\nfim da impressao do grafo.\n\nImpressão dos nomes:" );
+        System.out.println("\n\nfim da impressao do grafo.\n\nImpressão dos nomes:");
 
-        for(int i = 0; i < nomes.length; ++i) {
+        for (int i = 0; i < nomes.length; ++i) {
             System.out.print(i + ": " + nomes[i] + ", ");
         }
     }
 
     public int inDegree(int v) {
         int count = 0;
-        for(int i = 0; i < adj.length; ++i) {
-            if(adj[i][v] != Double.POSITIVE_INFINITY) {
+        for (int i = 0; i < adj.length; ++i) {
+            if (adj[i][v] != Double.POSITIVE_INFINITY) {
                 count++;
             }
         }
@@ -107,8 +117,8 @@ public class TGrafoRotulado extends Grafo{
 
     public int outDegree(int v) {
         int count = 0;
-        for(int i = 0; i < adj[v].length; ++i) {
-            if(adj[v][i] != Double.POSITIVE_INFINITY) {
+        for (int i = 0; i < adj[v].length; ++i) {
+            if (adj[v][i] != Double.POSITIVE_INFINITY) {
                 count++;
             }
         }
@@ -133,9 +143,11 @@ public class TGrafoRotulado extends Grafo{
     }
 
     public int isSimetrico() {
-        for(int i = 0; i < adj.length; ++i) {
-            for(int j = i + 1; j < adj[i].length; j++) {
-                if(adj[i][j] != adj[j][i]) {return 0;}
+        for (int i = 0; i < adj.length; ++i) {
+            for (int j = i + 1; j < adj[i].length; j++) {
+                if (adj[i][j] != adj[j][i]) {
+                    return 0;
+                }
             }
         }
 
@@ -143,9 +155,11 @@ public class TGrafoRotulado extends Grafo{
     }
 
     public int isSimetrico(int[][] adj) {
-        for(int i = 0; i < adj.length; ++i) {
-            for(int j = i + 1; j < adj[i].length; j++) {
-                if(adj[i][j] != adj[j][i]) {return 0;}
+        for (int i = 0; i < adj.length; ++i) {
+            for (int j = i + 1; j < adj[i].length; j++) {
+                if (adj[i][j] != adj[j][i]) {
+                    return 0;
+                }
             }
         }
 
@@ -156,9 +170,11 @@ public class TGrafoRotulado extends Grafo{
         File file = new File(nome_arquivo);
         Scanner scanner;
 
-        try{scanner = new Scanner(file);}
+        try {
+            scanner = new Scanner(file);
+        }
 
-        catch(FileNotFoundException e) {
+        catch (FileNotFoundException e) {
             System.out.println("Arquivo não encontado!");
             return null;
         }
@@ -168,7 +184,7 @@ public class TGrafoRotulado extends Grafo{
 
         double[][] adj = new double[v][v];
 
-        for(int i = 0; i < a; ++i) {
+        for (int i = 0; i < a; ++i) {
             String[] array = scanner.nextLine().strip().split(" ");
             int prim = Integer.parseInt(array[0]);
             int sec = Integer.parseInt(array[1]);
@@ -181,28 +197,30 @@ public class TGrafoRotulado extends Grafo{
     }
 
     public void removeV(int v) {
-        if(v > n-1) {
+        if (v > n - 1) {
             System.out.println("Vértice nao existente!");
             return;
         }
 
-        double[][] new_adj = new double[n-1][n-1];
-        String[] newNomes = new String[n-1];
+        double[][] new_adj = new double[n - 1][n - 1];
+        String[] newNomes = new String[n - 1];
         int ni = 0;
         int nj = 0;
         int nk = 0;
         int count = degree(v);
 
-        for(int i = 0; i < adj.length; ++i) {
-            for(int j = 0; j < adj[i].length; ++j) {
-                if(i == v) {
+        for (int i = 0; i < adj.length; ++i) {
+            for (int j = 0; j < adj[i].length; ++j) {
+                if (i == v) {
                     ni--;
                     break;
                 }
 
-                else if(j == v) {continue;}
+                else if (j == v) {
+                    continue;
+                }
 
-                else{
+                else {
                     new_adj[ni][nj] = adj[i][j];
                     nj++;
                 }
@@ -211,7 +229,7 @@ public class TGrafoRotulado extends Grafo{
             ni++;
             nj = 0;
 
-            if(i != v) {
+            if (i != v) {
                 newNomes[nk] = nomes[i];
                 nk++;
             }
@@ -224,9 +242,11 @@ public class TGrafoRotulado extends Grafo{
     }
 
     public int isCompleto() {
-        for(int i = 0; i < adj.length; ++i) {
-            for(int j = 0; j < adj.length; ++j) {
-                if(i != j && adj[i][j] == Double.POSITIVE_INFINITY) {return 0;}
+        for (int i = 0; i < adj.length; ++i) {
+            for (int j = 0; j < adj.length; ++j) {
+                if (i != j && adj[i][j] == Double.POSITIVE_INFINITY) {
+                    return 0;
+                }
             }
         }
 
@@ -235,13 +255,15 @@ public class TGrafoRotulado extends Grafo{
 
     public double[][] complemento() {
         double[][] complemen = new double[n][n];
-        for(int i = 0; i < adj.length; ++i) {
-            for(int j = 0; j < adj.length; ++j) {
-                if(i != j && adj[i][j] != Double.POSITIVE_INFINITY) {
+        for (int i = 0; i < adj.length; ++i) {
+            for (int j = 0; j < adj.length; ++j) {
+                if (i != j && adj[i][j] != Double.POSITIVE_INFINITY) {
                     complemen[i][j] = Double.POSITIVE_INFINITY;
                 }
 
-                else if(i != j && adj[i][j] == Double.POSITIVE_INFINITY) {complemen[i][j] = -1;}
+                else if (i != j && adj[i][j] == Double.POSITIVE_INFINITY) {
+                    complemen[i][j] = -1;
+                }
             }
         }
 
@@ -252,20 +274,24 @@ public class TGrafoRotulado extends Grafo{
         int[] vet = new int[adj.length];
         conectividadeAux(vet, 0);
 
-        for(int i = 0; i < adj.length; ++i) {
-            if(vet[i] == 0) {return 1;}
+        for (int i = 0; i < adj.length; ++i) {
+            if (vet[i] == 0) {
+                return 1;
+            }
         }
 
         return 0;
     }
 
     public void conectividadeAux(int[] vet, int v) {
-        if (vet[v] == 0) {vet[v] = 1;}
+        if (vet[v] == 0) {
+            vet[v] = 1;
+        }
         int[] fechosTransitivos = geraFechos(v, adj);
         int[] fechosIntransitivos = geraFechos(v, inverte());
 
         for (int j = 0; j < adj.length; ++j) {
-            if((fechosTransitivos[j] == 1 || fechosIntransitivos[j] == 1) && vet[j] == 0) {
+            if ((fechosTransitivos[j] == 1 || fechosIntransitivos[j] == 1) && vet[j] == 0) {
                 conectividadeAux(vet, j);
             }
         }
@@ -278,17 +304,21 @@ public class TGrafoRotulado extends Grafo{
     }
 
     private void geraFechosAux(int[] fechos, int v, double[][] matriz) {
-        if(fechos[v] == 0) {fechos[v] = 1;}
+        if (fechos[v] == 0) {
+            fechos[v] = 1;
+        }
 
-        for(int i = 0; i < fechos.length; ++i) {
-            if(matriz[v][i] != Double.POSITIVE_INFINITY && fechos[i] == 0) {geraFechosAux(fechos, i, matriz);}
+        for (int i = 0; i < fechos.length; ++i) {
+            if (matriz[v][i] != Double.POSITIVE_INFINITY && fechos[i] == 0) {
+                geraFechosAux(fechos, i, matriz);
+            }
         }
     }
 
     private double[][] inverte() {
         double[][] inverso = new double[adj.length][adj.length];
-        for(int i = 0; i < adj.length; ++i) {
-            for(int j = 0; j < adj.length; ++j) {
+        for (int i = 0; i < adj.length; ++i) {
+            for (int j = 0; j < adj.length; ++j) {
                 inverso[j][i] = adj[i][j];
             }
         }
@@ -297,10 +327,15 @@ public class TGrafoRotulado extends Grafo{
     }
 
     public int grauConexidade() {
-        if(grauTres() == 0) {return 3;}
-        else if(grauDois() == 0) {return 2;}
-        else if(conectividadade() == 0) {return 1;}
-        else if(conectividadade() == 1) {return 0;}
+        if (grauTres() == 0) {
+            return 3;
+        } else if (grauDois() == 0) {
+            return 2;
+        } else if (conectividadade() == 0) {
+            return 1;
+        } else if (conectividadade() == 1) {
+            return 0;
+        }
         return -1;
     }
 
@@ -308,8 +343,8 @@ public class TGrafoRotulado extends Grafo{
         for (int i = 0; i < adj.length; ++i) {
             int[] fechosTransitivos = geraFechos(i, adj);
             int[] fechosIntransitivos = geraFechos(i, inverte());
-            for(int j = 0; j < adj.length; ++j) {
-                if(fechosTransitivos[j] == 0 || fechosIntransitivos[j] == 0) {
+            for (int j = 0; j < adj.length; ++j) {
+                if (fechosTransitivos[j] == 0 || fechosIntransitivos[j] == 0) {
                     return 1;
                 }
             }
@@ -322,8 +357,8 @@ public class TGrafoRotulado extends Grafo{
         for (int i = 0; i < adj.length; ++i) {
             int[] fechosTransitivos = geraFechos(i, adj);
             int[] fechosIntransitivos = geraFechos(i, inverte());
-            for(int j = 0; j < adj.length; ++j) {
-                if(fechosTransitivos[j] == 0 && fechosIntransitivos[j] == 0) {
+            for (int j = 0; j < adj.length; ++j) {
+                if (fechosTransitivos[j] == 0 && fechosIntransitivos[j] == 0) {
                     return 1;
                 }
             }
@@ -334,8 +369,10 @@ public class TGrafoRotulado extends Grafo{
 
     int max(int[] vet) {
         int n = vet[0];
-        for(int i = 1; i < vet.length; ++i) {
-            if(vet[i] > n) {n = vet[i];}
+        for (int i = 1; i < vet.length; ++i) {
+            if (vet[i] > n) {
+                n = vet[i];
+            }
         }
 
         return n;
@@ -344,11 +381,11 @@ public class TGrafoRotulado extends Grafo{
     public TGrafoRotulado grafoReduzido() {
         int[] vet = new int[adj.length];
         int count = 1;
-        for(int i = 0; i < adj.length; ++i) {
+        for (int i = 0; i < adj.length; ++i) {
             int[] direto = geraFechos(i, adj);
             int[] indireto = geraFechos(i, inverte());
-            for(int j = 0; j < adj.length; ++j) {
-                if(direto[j] == 1 && indireto[j] == 1 && vet[j] == 0) {
+            for (int j = 0; j < adj.length; ++j) {
+                if (direto[j] == 1 && indireto[j] == 1 && vet[j] == 0) {
                     vet[j] = count;
                 }
             }
@@ -358,15 +395,15 @@ public class TGrafoRotulado extends Grafo{
 
         TGrafoRotulado grafo = new TGrafoRotulado(max(vet));
 
-        for(int i = 0; i < adj.length; ++i) {
+        for (int i = 0; i < adj.length; ++i) {
             int[] direto = geraFechos(i, adj);
             int[] indireto = geraFechos(i, inverte());
             for (int j = 0; j < adj.length; ++j) {
-                if(direto[j] == 1 && indireto[j] == 0 && adj[i][j] != Double.POSITIVE_INFINITY) {
+                if (direto[j] == 1 && indireto[j] == 0 && adj[i][j] != Double.POSITIVE_INFINITY) {
                     grafo.insereA(vet[i] - 1, vet[j] - 1, -1);
                 }
 
-                else if(direto[j] == 0 && indireto[j] == 1 && adj[j][i] != Double.POSITIVE_INFINITY) {
+                else if (direto[j] == 0 && indireto[j] == 1 && adj[j][i] != Double.POSITIVE_INFINITY) {
                     grafo.insereA(vet[j] - 1, vet[i] - 1, -1);
                 }
             }
@@ -382,10 +419,10 @@ public class TGrafoRotulado extends Grafo{
         System.out.print("\nPercurso em profundidade: " + n);
         vet[n] = 1;
         pilha.push(n);
-        while(!pilha.isEmpty()) {
+        while (!pilha.isEmpty()) {
             n = pilha.pop();
-            for(int m = 0; m < adj.length; ++m) {
-                if(adj[n][m] != Double.POSITIVE_INFINITY && vet[m] == 0) {
+            for (int m = 0; m < adj.length; ++m) {
+                if (adj[n][m] != Double.POSITIVE_INFINITY && vet[m] == 0) {
                     System.out.print(" " + m);
                     vet[m] = 1;
                     pilha.push(n);
@@ -402,10 +439,10 @@ public class TGrafoRotulado extends Grafo{
         System.out.print("\nPercurso em largura: " + n);
         vet[n] = 1;
         fila.enqueue(n);
-        while(!fila.qIsEmpty()) {
+        while (!fila.qIsEmpty()) {
             n = fila.dequeue();
-            for(int m = 0; m < adj.length; ++m) {
-                if(adj[n][m] != Double.POSITIVE_INFINITY && vet[m] == 0) {
+            for (int m = 0; m < adj.length; ++m) {
+                if (adj[n][m] != Double.POSITIVE_INFINITY && vet[m] == 0) {
                     System.out.print(" " + m);
                     vet[m] = 1;
                     fila.enqueue(m);
@@ -419,17 +456,17 @@ public class TGrafoRotulado extends Grafo{
         double[][] newAdj = new double[newN][newN];
         String[] newNomes = new String[newN];
 
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < n; ++j) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
                 newAdj[i][j] = adj[i][j];
-                newAdj[newN-1][i] = Double.POSITIVE_INFINITY;
-                newAdj[i][newN-1] = Double.POSITIVE_INFINITY;
+                newAdj[newN - 1][i] = Double.POSITIVE_INFINITY;
+                newAdj[i][newN - 1] = Double.POSITIVE_INFINITY;
             }
 
             newNomes[i] = nomes[i];
         }
 
-        newAdj[newN-1][newN-1] = Double.POSITIVE_INFINITY;
+        newAdj[newN - 1][newN - 1] = Double.POSITIVE_INFINITY;
         newNomes[n] = nome;
         this.n = newN;
         this.adj = newAdj;
