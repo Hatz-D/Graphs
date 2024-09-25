1baf0c5b24059781e85ce1c1452e3eaf90db710f
nicolasmelnik <nmelnik58@gmail.com>
2024-09-24 23:14:50 -0300

    Atualizando

--- src/GrafoMatriz/TGrafoNDPeso.java
+++ src/GrafoMatriz/TGrafoNDPeso.java
@@ -1,6 +1,7 @@
 /*
-Diogo Lourenzon Hatz     - 10402406
-Nicolas Fernandes Melnik - 10402170
+Diogo Lourenzon Hatz     - RA: 10402406
+Eduardo Marui de Camargo - RA: 10400734
+Nicolas Fernandes Melnik - RA: 10402170
 
 Classe: Classe relativa ao grafo não direcionado com peso nos vértices
 
@@ -9,28 +10,29 @@ Mudanças:
 - Separação das classes de grafo em classes distintas e criação de classe abstrata Grafos- Diogo Hatz, 11/09/2024
 - Criação da classe Projeto para métodos de manipulação de arquivos - Diogo Hatz, 11/09/2024
 - Garantir consistência dos métodos das classes dos grafos - Nicolas Melnik 13/09/2024
+- Adicionando mapeamento da europa no arquivo grafo.txt - Nicolas Melnik e Eduardo Marui 24/09/2024
 */
 
 package GrafoMatriz;
 
 public class TGrafoNDPeso extends Grafo {
     // Atributos Privados
-    protected int[][] adj; //matriz de adjacência
+    protected int[][] adj; // matriz de adjacência
     protected double[] pesos;
 
     // Métodos Públicos
-    public TGrafoNDPeso(int n) {  // construtor
+    public TGrafoNDPeso(int n) { // construtor
         this.n = n;
         // No início dos tempos não há arestas
         this.m = 0;
         // alocação da matriz do TGrafo
-        this.adj = new int [n][n];
+        this.adj = new int[n][n];
         this.pesos = new double[n];
         this.nomes = new String[n];
 
         // Inicia a matriz com zeros
-        for(int i = 0; i< n; i++) {
-            for(int j = 0; j< n; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
                 this.adj[i][j] = 0;
             }
             pesos[i] = Double.POSITIVE_INFINITY;
@@ -44,77 +46,90 @@ public class TGrafoNDPeso extends Grafo {
         this.nomes = new String[n];
         int count = 0;
 
-        for(int i = 0; i < adj.length; ++i) {
-            for(int j = i+1; j < adj.length; j++) {
-                if(adj[i][j] != 0) {count++;}
+        for (int i = 0; i < adj.length; ++i) {
+            for (int j = i + 1; j < adj.length; j++) {
+                if (adj[i][j] != 0) {
+                    count++;
+                }
             }
         }
         this.m = count;
     }
 
-    public int[][] getAdj() {return adj;}
+    public int[][] getAdj() {
+        return adj;
+    }
 
-    public void setAdj(int[][] adj) {this.adj = adj;}
+    public void setAdj(int[][] adj) {
+        this.adj = adj;
+    }
 
-    public double[] getPesos() {return pesos;}
+    public double[] getPesos() {
+        return pesos;
+    }
 
-    public void setPesos(double[] pesos) {this.pesos = pesos;}
+    public void setPesos(double[] pesos) {
+        this.pesos = pesos;
+    }
 
     // Insere uma aresta no Grafo tal que
     // v é adjacente a w
     public void insereA(int v, int w) {
         // testa se nao temos a aresta
-        if(adj[v][w] == 0 && adj[w][v] == 0) {
+        if (adj[v][w] == 0 && adj[w][v] == 0) {
             adj[v][w] = 1;
             adj[w][v] = 1;
             ++m; // atualiza qtd arestas
         }
 
-        //else{System.out.println("\nAresta já existente!");}
+        // else{System.out.println("\nAresta já existente!");}
     }
 
     // remove uma aresta v->w do Grafo
     public void removeA(int v, int w) {
         // testa se temos a aresta
-        if(adj[v][w] == 1 && adj[w][v] == 1){
+        if (adj[v][w] == 1 && adj[w][v] == 1) {
             adj[v][w] = 0;
             adj[w][v] = 0;
             --m; // atualiza qtd arestas
         }
 
-        else{System.out.println("\nAresta não existente!");}
+        else {
+            System.out.println("\nAresta não existente!");
+        }
     }
 
     // Apresenta o Grafo contendo
     // número de vértices, arestas
     // e a matriz de adjacência obtida
     public void show() {
-        System.out.println("V: " + n );
-        System.out.println("A: " + m );
-        for( int i=0; i < n; i++){
+        System.out.println("V: " + n);
+        System.out.println("A: " + m);
+        for (int i = 0; i < n; i++) {
             System.out.print("\n");
-            for( int w=0; w < n; w++)
-                if(adj[i][w] == 1)
+            for (int w = 0; w < n; w++)
+                if (adj[i][w] == 1)
                     System.out.print("Adj[" + i + "," + w + "]= 1" + " ");
-                else System.out.print("Adj[" + i + "," + w + "]= 0" + " ");
+                else
+                    System.out.print("Adj[" + i + "," + w + "]= 0" + " ");
         }
-        System.out.println("\n\nfim da impressao do grafo.\n\nImpressão dos nomes:" );
+        System.out.println("\n\nfim da impressao do grafo.\n\nImpressão dos nomes:");
 
-        for(int i = 0; i < nomes.length; ++i) {
+        for (int i = 0; i < nomes.length; ++i) {
             System.out.print(i + ": " + nomes[i] + ", ");
         }
 
         System.out.println("Impressão dos pesos dos vértices:");
 
-        for(int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i) {
             System.out.print(i + ": " + pesos[i] + ", ");
         }
     }
 
     public int degree(int v) {
         int count = 0;
-        for(int i = 0; i < adj.length; ++i) {
-            if(adj[i][v] != 0 && adj[v][i] != 0) {
+        for (int i = 0; i < adj.length; ++i) {
+            if (adj[i][v] != 0 && adj[v][i] != 0) {
                 count++;
             }
         }
@@ -123,30 +138,32 @@ public class TGrafoNDPeso extends Grafo {
     }
 
     public void removeV(int v) {
-        if(v > n-1) {
+        if (v > n - 1) {
             System.out.println("Vértice nao existente!");
             return;
         }
 
-        int[][] new_adj = new int[n-1][n-1];
-        String[] newNomes = new String[n-1];
-        double[] newPesos = new double[n-1];
+        int[][] new_adj = new int[n - 1][n - 1];
+        String[] newNomes = new String[n - 1];
+        double[] newPesos = new double[n - 1];
 
         int ni = 0;
         int nj = 0;
         int nk = 0;
         int count = degree(v);
 
-        for(int i = 0; i < adj.length; ++i) {
-            for(int j = 0; j < adj.length; ++j) {
-                if(i == v) {
+        for (int i = 0; i < adj.length; ++i) {
+            for (int j = 0; j < adj.length; ++j) {
+                if (i == v) {
                     ni--;
                     break;
                 }
 
-                else if(j == v) {continue;}
+                else if (j == v) {
+                    continue;
+                }
 
-                else{
+                else {
                     new_adj[ni][nj] = adj[i][j];
                     nj++;
                 }
@@ -155,7 +172,7 @@ public class TGrafoNDPeso extends Grafo {
             ni++;
             nj = 0;
 
-            if(i != v) {
+            if (i != v) {
                 newNomes[nk] = nomes[i];
                 newPesos[nk] = pesos[i];
                 nk++;
@@ -171,9 +188,11 @@ public class TGrafoNDPeso extends Grafo {
     }
 
     public int isCompleto() {
-        for(int i = 0; i < adj.length; ++i) {
-            for(int j = 0; j < adj.length; ++j) {
-                if(i != j && adj[i][j] != 1) {return 0;}
+        for (int i = 0; i < adj.length; ++i) {
+            for (int j = 0; j < adj.length; ++j) {
+                if (i != j && adj[i][j] != 1) {
+                    return 0;
+                }
             }
         }
 
@@ -182,10 +201,13 @@ public class TGrafoNDPeso extends Grafo {
 
     public int[][] complemento() {
         int[][] complemen = new int[n][n];
-        for(int i = 0; i < adj.length; ++i) {
-            for(int j = 0; j < adj.length; ++j) {
-                if(i != j && adj[i][j] == 1) {complemen[i][j] = 0;}
-                else if(i != j && adj[i][j] == 0) {complemen[i][j] = 1;}
+        for (int i = 0; i < adj.length; ++i) {
+            for (int j = 0; j < adj.length; ++j) {
+                if (i != j && adj[i][j] == 1) {
+                    complemen[i][j] = 0;
+                } else if (i != j && adj[i][j] == 0) {
+                    complemen[i][j] = 1;
+                }
             }
         }
 
@@ -196,19 +218,23 @@ public class TGrafoNDPeso extends Grafo {
         int[] vet = new int[adj.length];
         conectividadeAux(vet, 0);
 
-        for(int i = 0; i < adj.length; ++i) {
-            if(vet[i] == 0) {return 1;}
+        for (int i = 0; i < adj.length; ++i) {
+            if (vet[i] == 0) {
+                return 1;
+            }
         }
 
         return 0;
     }
 
     public void conectividadeAux(int[] vet, int v) {
-        if (vet[v] == 0) {vet[v] = 1;}
+        if (vet[v] == 0) {
+            vet[v] = 1;
+        }
         int[] fechos = geraFechos(v);
 
         for (int j = 0; j < adj.length; ++j) {
-            if(fechos[j] == 1 && vet[j] == 0) {
+            if (fechos[j] == 1 && vet[j] == 0) {
                 conectividadeAux(vet, j);
             }
         }
@@ -221,17 +247,21 @@ public class TGrafoNDPeso extends Grafo {
     }
 
     private void geraFechosAux(int[] fechos, int v) {
-        if(fechos[v] == 0) {fechos[v] = 1;}
+        if (fechos[v] == 0) {
+            fechos[v] = 1;
+        }
 
-        for(int i = 0; i < fechos.length; ++i) {
-            if(adj[v][i] != 0 && fechos[i] == 0) {geraFechosAux(fechos, i);}
+        for (int i = 0; i < fechos.length; ++i) {
+            if (adj[v][i] != 0 && fechos[i] == 0) {
+                geraFechosAux(fechos, i);
+            }
         }
     }
 
     private int[][] inverte() {
         int[][] inverso = new int[adj.length][adj.length];
-        for(int i = 0; i < adj.length; ++i) {
-            for(int j = 0; j < adj.length; ++j) {
+        for (int i = 0; i < adj.length; ++i) {
+            for (int j = 0; j < adj.length; ++j) {
                 inverso[j][i] = adj[i][j];
             }
         }
@@ -246,10 +276,10 @@ public class TGrafoNDPeso extends Grafo {
         System.out.print("\nPercurso em profundidade: " + n);
         vet[n] = 1;
         pilha.push(n);
-        while(!pilha.isEmpty()) {
+        while (!pilha.isEmpty()) {
             n = pilha.pop();
-            for(int m = 0; m < adj.length; ++m) {
-                if(adj[n][m] == 1 && vet[m] == 0) {
+            for (int m = 0; m < adj.length; ++m) {
+                if (adj[n][m] == 1 && vet[m] == 0) {
                     System.out.print(" " + m);
                     vet[m] = 1;
                     pilha.push(n);
@@ -266,10 +296,10 @@ public class TGrafoNDPeso extends Grafo {
         System.out.print("\nPercurso em largura: " + n);
         vet[n] = 1;
         fila.enqueue(n);
-        while(!fila.qIsEmpty()) {
+        while (!fila.qIsEmpty()) {
             n = fila.dequeue();
-            for(int m = 0; m < adj.length; ++m) {
-                if(adj[n][m] == 1 && vet[m] == 0) {
+            for (int m = 0; m < adj.length; ++m) {
+                if (adj[n][m] == 1 && vet[m] == 0) {
                     System.out.print(" " + m);
                     vet[m] = 1;
                     fila.enqueue(m);
@@ -284,18 +314,18 @@ public class TGrafoNDPeso extends Grafo {
         String[] newNomes = new String[newN];
         double[] newPesos = new double[newN];
 
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < n; ++j) {
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
                 newAdj[i][j] = adj[i][j];
-                newAdj[newN-1][i] = 0;
-                newAdj[i][newN-1] = 0;
+                newAdj[newN - 1][i] = 0;
+                newAdj[i][newN - 1] = 0;
             }
 
             newNomes[i] = nomes[i];
             newPesos[i] = pesos[i];
         }
 
-        newAdj[newN-1][newN-1] = 0;
+        newAdj[newN - 1][newN - 1] = 0;
         newNomes[n] = nome;
         newPesos[n] = peso;
         this.n = newN;
